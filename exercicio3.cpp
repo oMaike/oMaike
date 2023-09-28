8ce484735308452157569a57eb49d07a7d747329
Maike G. Rosa <137224991+oMaike@users.noreply.github.com>
2023-09-28 16:32:16 -0300

    Exercicio3

---
+++ exercicio3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int (int n1, int n2, int &r){
+	
+	resto = n1 % n2;
+	return n1/n2;
+}
+	
+	
+
+
+
+
+int main(){
+	
+	int valor1, valor2,quociente, resto;
+	cin >> valor1, valor2 ;
+	quociente = funcao (v1, v2, resto);
+	cout << quociente << " " << resto;
+	
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+/*faca uma funcao que recebe dois valores inteiros e retorna o quociente e o resto da divisao
+para o programa principal.os valores do quociente e resto devem ser impressos no programa principal*/
